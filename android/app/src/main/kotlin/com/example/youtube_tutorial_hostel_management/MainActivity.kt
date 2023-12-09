0242262b329bc2899404c1bfe1115672ecc42381
banku27 <pankajparihar123321@gmail.com>
2023-12-09 15:44:07 +0530

    project setup

---
+++ android/app/src/main/kotlin/com/example/youtube_tutorial_hostel_management/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.youtube_tutorial_hostel_management
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Youtube Tutorial Flutter App',
+    );
+  }
+}
